85253a869c1e66063096c9a729048f4132fde5d8
Max Albrecht <max.albrecht@zhdk.ch>
2023-04-08 00:01:11 +0200

    refactor: simply types

--- src/react-let.tsx
+++ src/react-let.tsx
@@ -1,5 +1,5 @@
 export type LetProps<P> = {
-  children: (props: Pick<P, Exclude<keyof P, "children">>) => JSX.Element;
+  children: (props: Omit<P, "children">) => JSX.Element;
 } & P;
 
 export function Let<P extends Record<string, unknown>>({ children, ...ownProps }: LetProps<P>): JSX.Element {
