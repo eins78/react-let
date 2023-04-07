718cb8d530cdcd430b7e36eb431308be578bc0a1
Max Albrecht <max.albrecht@zhdk.ch>
2023-04-07 21:55:24 +0200

    feat: add usage example

--- src/example/App.tsx
+++ src/example/App.tsx
@@ -1,22 +1,11 @@
-# `react-let`
-
-Like `(let)` in Lisp, [Clojure](https://clojuredocs.org/clojure.core/let), etc., but for React/JSX.
-
-Makes it easier to declare variables deep inside a JSX tree,
-which are then passed to a `children` render function.
-
-When used with TypeScript, the render function arguments are infered from the props given to `<Let>`.
-
-## Usage Examples
-
-```tsx
-import React from "react";
+import { useState } from "react";
 import { Let } from "../main";
 
 export const ExampleMemo = () => (
   <div>
     <Let result={"expensive-calculation-result"} another={42}>
-      {({ result, another }) => ( // TypeScript infered: (parameter) result: string; (parameter) another: number
+      {({ result, another }) => (
+        // ^? (parameter) result: string; (parameter) another: number
         <p>
           {result}. {another}.
         </p>
@@ -28,7 +17,8 @@ export const ExampleMemo = () => (
 export const ExampleInput = () => (
   <div>
     <Let id={"myId"}>
-      {({ id }) => ( // TypeScript infered: (parameter) id: string
+      {({ id }) => (
+        // ^? (parameter) id: string
         <>
           <label htmlFor={id}>{id}</label>
           <input id={id} />
@@ -50,7 +40,8 @@ export const ExampleAsync = () => {
         "error!"
       ) : (
         <Let data={fetchResult.data}>
-          {({ data }) => ( // TypeScript infered: (parameter) data: FetchData
+          {({ data }) => (
+            // ^? (parameter) data: FetchData
             <>
               <pre>{JSON.stringify(data)}</pre>
             </>
@@ -60,14 +51,17 @@ export const ExampleAsync = () => {
     </div>
   );
 };
-```
 
-More examples can be found in `src/example/App.tsx`,
-and be can be run using:
+function App() {
+  return (
+    <div className="App">
+      <p>hello world!</p>
+      <hr />
+      <ExampleMemo />
+      <hr />
+      <ExampleInput />
+    </div>
+  );
+}
 
-```shell
-npm ci
-npm run dev
-```
-
-The same command could also used for development (but no work is currently planned as the component is feature-complete).
+export default App;
